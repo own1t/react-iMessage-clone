8d8f74171aa28d5b7d3d7a536634c208220a8ae9
Ryan Kim <iawal86@gmail.com>
2020-10-10 10:08:38 -0700

    created messenger page component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 // React
 import React from "react";
 
+// Views
+import Messenger from "./views/Messenger";
+
+// Components
+
 // CSS
 import "./App.css";
 
@@ -9,6 +14,8 @@ function App() {
     // BEM naming convention
     <div className="app">
       <h1>Hello World</h1>
+
+      <Messenger />
     </div>
   );
 }
---
+++ src/views/Messenger.js
@@ -0,0 +1,11 @@
+// React
+import React from "react";
+
+// CSS
+import "./Messenger.css";
+
+const IMessage = () => {
+  return <div className="messenger"></div>;
+};
+
+export default IMessage;
